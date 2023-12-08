02f8146a351d3a5849485a3b9c174aef9ff5f7b5
hrbaleja <hiteshbaleja@gmail.com>
2023-12-08 18:55:09 +0530

    New Product

--- src/components/home/HomeCategory.js
+++ src/components/home/HomeCategory.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { Grid, Card, CardContent, CardMedia, Typography, styled } from '@mui/material';
 import DryFruit from '../Images/Category/DryFruit.jpg'
 import Seed from '../Images/Category/Seed.jpg'
+import Chocolate from '../Images/Category/Chocolate.jpg'
+import DriedFruit from '../Images/Category/DriedFruit.jpg'
+import Juice from '../Images/Category/Juice.jpg'
+import Bite from '../Images/Category/Bite.jpg'
+
+
 
 const useStyles = styled((theme) => ({
     root: {
@@ -19,16 +25,17 @@ const useStyles = styled((theme) => ({
     },
     cardContent: {
         flexGrow: 1,
+       
     },
 }));
 
 const items = [
     { id: 1, name: 'Dry Fruits', imageUrl: DryFruit },
-    { id: 2, name: 'Seed', imageUrl: Seed },
-    { id: 3, name: 'Energy Drink', imageUrl: 'https://mychocolate-api.vercel.app/api/v1/uploads/get-image/image-1698233867179.png' },
-    { id: 4, name: 'Pistachio', imageUrl: 'https://mychocolate-api.vercel.app/api/v1/uploads/get-image/image-1698232690841.jpg' },
-    { id: 5, name: 'Coated Almonds', imageUrl: 'https://mychocolate-api.vercel.app/api/v1/uploads/get-image/image-1698231872106.jpg' },
-    { id: 6, name: 'Eviya Plup N Juice', imageUrl: 'https://mychocolate-api.vercel.app/api/v1/uploads/get-image/image-1698233916426.png' },
+    { id: 2, name: 'Chocolate', imageUrl: Chocolate },
+    { id: 3, name: 'Seed', imageUrl: Seed },
+    { id: 4, name: 'DriedFruit', imageUrl: DriedFruit },
+    { id: 5, name: 'Juice', imageUrl:Juice},
+    { id: 6, name: 'Choco Bite', imageUrl: Bite },
 ];
 
 const HomeCategory = () => {
--- src/components/home/HomeProduct.js
+++ src/components/home/HomeProduct.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Grid, Card, CardContent, CardMedia, Typography, styled } from '@mui/material';
+import { getProducts } from '../services/productService';
 
 const useStyles = styled((theme) => ({
     root: {
@@ -20,49 +21,62 @@ const useStyles = styled((theme) => ({
     },
 }));
 
-const items = [
-    { id: 1, name: 'Cranberrie', imageUrl: 'https://mychocolate-api.vercel.app/api/v1/uploads/get-image/image-1698233122943.jpg' },
-    { id: 2, name: 'Strawberrie', imageUrl: 'https://mychocolate-api.vercel.app/api/v1/uploads/get-image/image-1698233078418.jpg' },
-    { id: 3, name: 'Energy Drink', imageUrl: 'https://mychocolate-api.vercel.app/api/v1/uploads/get-image/image-1698233867179.png' },
-    { id: 4, name: 'Pistachio', imageUrl: 'https://mychocolate-api.vercel.app/api/v1/uploads/get-image/image-1698232690841.jpg' },
-    { id: 5, name: 'Coated Almonds', imageUrl: 'https://mychocolate-api.vercel.app/api/v1/uploads/get-image/image-1698231872106.jpg' },
-    { id: 6, name: 'Eviya Plup N Juice', imageUrl: 'https://mychocolate-api.vercel.app/api/v1/uploads/get-image/image-1698233916426.png' },
-];
+const HomeRecent = () => {
+  const classes = useStyles();
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-const PropularProduct = () => {
-    const classes = useStyles();
-    return (
-        <div   style={{ backgroundColor: '#f7f7f7',  padding: '0 1rem 2.5rem 1rem' }}>
-            <Grid container spacing={2} mt={2} mb={2} justifyContent='center' data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="200" >
-                <Typography variant="h4" component="h2" style={{ padding: '10px 0 0 20px', textAlign: 'center', fontFamily: 'Lora' }}>
-                    EXPLORE OUR  PRODUCT
-                </Typography>
-            </Grid>
-            <hr data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="200" />
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getProducts();
+        setProducts(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+        setLoading(false);
+      }
+    };
 
-            <Grid container spacing={2}>
-                {items.map((item) => (
-                    <Grid item xs={12} sm={6} md={4} lg={2} key={item.id}>
-                        <Card className={classes.card}>
-                            <CardMedia
-                                className={classes.cardMedia}
-                                component="img"
-                                height="200"
-                                width="180"
-                                image={item.imageUrl}
-                                alt={item.name}
-                            />
-                            <CardContent className={classes.cardContent}>
-                                <Typography gutterBottom variant="h6" component="div" style={{ color: '#917236', fontFamily: 'koho' }}>
-                                    {item.name}
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                ))}
+    fetchData();
+  }, []); 
+
+  return (
+    <div style={{ backgroundColor: '#f7f7f7', padding: '0 1rem 2.5rem 1rem' }}>
+      <Grid container spacing={2} mt={2} mb={2} justifyContent='center' data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="200" >
+        <Typography variant="h4" component="h2" style={{ padding: '10px 0 0 20px', textAlign: 'center', fontFamily: 'Lora' }}>
+        EXPLORE OUR  PRODUCT
+        </Typography>
+      </Grid>
+      <hr data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="200" />
+
+      <Grid container spacing={2}>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+            products.slice(0,6).map((product) => (
+            <Grid item xs={12} sm={6} md={4} lg={2} key={product._id}>
+              <Card className={classes.card}>
+                <CardMedia
+                  className={classes.cardMedia}
+                  component="img"
+                  height="200"
+                  width="180"
+                  image={`https://mychocolate-api.vercel.app/api/v1/uploads/get-image/${product.image}`}
+                  alt={product.name}
+                />
+                <CardContent className={classes.cardContent}>
+                  <Typography gutterBottom variant="P1" component="div" style={{ color: '#917236', fontFamily: 'koho' }}>
+                    {product.name}
+                  </Typography>
+                </CardContent>
+              </Card>
             </Grid>
-        </div>
-    );
+          ))
+        )}
+      </Grid>
+    </div>
+  );
 };
 
-export default PropularProduct;
+export default HomeRecent;
